d99ddeb2f860039be847a5dde8181e870d2482a6
Eduardo Araujo dos Santos <eduardo.santos@rede.ulbra.br>
2026-09-13 12:30:21 -0300

    Atualizando api para retorna erros e diferenciar os 4 estados

--- js/api.js
+++ js/api.js
@@ -2,13 +2,18 @@ async function carregarTarefas(){
     try{
         const response = await fetch("./../dados.json")
         
-        if (!(response.ok)) return new Error(`Error: ${e.status}`);
+        if (!response.ok) return new Error("Erro ao carregar tarefas: " + response.statusText);
 
         const tarefas = await response.json();
         
+        const tamArr = tarefas.tarefas.length;
+
+        if (tamArr == 0) return new Error(`Nenhuma tarefa encontrada`);
+        
+
         return tarefas
     }catch(e){
-        console.log(e);
+        return new Error(e.message);
     }
 }
 
