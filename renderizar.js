08982aaad176c1d69907f15ecc260ed3a2c79a04
Eduardo Araujo dos Santos <eduardo.santos@rede.ulbra.br>
2026-09-15 00:36:50 -0300

    Adicionando a funcao iniciar para deixar o programa mais limpo e usando estado.js para adicionar os cartões na aplicação

--- renderizar.js
+++ renderizar.js
@@ -1,17 +1,39 @@
-import {tarefas} from "./js/api.js"; 
+import {redenrizarEstado} from "./js/estado.js"
+import {carregarTarefas} from "./js/api.js"
 
-const quadro = document.querySelector("#projetos");
+async function iniciarQuadro(){
+
+    redenrizarEstado("carregando");
+
+    try{
+        const tarefas = await carregarTarefas();
+
+        console.log(tarefas);
+
+        if (tarefas.length == 0){
+            redenrizarEstado("vazio");
+        } else{
+            redenrizarEstado("sucesso", tarefas); 
+        }
+    }catch(e){
+        console.log(e)
+        renderizarEstado("erro")
+    }
+}
+
+iniciarQuadro();
 
 
+/*
 function criarCartao(tarefa) {
     const cartao = document.createElement("article");
-    cartao.id = tarefa.status;
+    cartao.classList.add(tarefa.status);
     
     const titulo = document.createElement("h3");
     titulo.textContent = tarefa.titulo;
 
     const button = document.createElement("button");
-    button.class = "cardButton";
+    button.classList.add("cardButton");
     button.innerText = "X";
 
     const p = document.createElement("p");
@@ -20,33 +42,50 @@ function criarCartao(tarefa) {
 
     const status = document.createElement("p");
     let prioridade = tarefa.prioridade;
-    status.innerHTML = `<p><span class="nivelPrioridade ${tarefa}">${tarefa.charAt(0).toUpperCase() + tarefa.slice(1)}</span></p>`;
+    status.innerHTML = `<p><span class="nivelPrioridade ${tarefa.prioridade}">${captalizer(tarefa.prioridade)}</span></p>`;
 
     
     cartao.append(titulo);
+    cartao.append(button);
     cartao.append(p);
     cartao.append(status);
 
     return cartao;
 }
+*/
+
+/*
+function carregarTarefas(cards) {
+    const quadro = document.querySelector("#projetos");
+    const quadroAfazer = quadro.querySelector("#aFazer");
+    const quadroEmAndamento = quadro.querySelector("#EmAndamento");
+    const quadroEmRevisao = quadro.querySelector("#EmRevisao");
+    const quadroConcluido = quadro.querySelector("#Concluido");
 
 
+    for(let card of cards) {
+        const li = document.createElement("li");  //criando li e adicionando o article dentro dela
+        li.append(card);
 
-quadro.addEventListener("click", (evento) => {
-    if (!(evento.target instanceof Element)) return;
-    const botao = evento.target.closest('button[data-acao="ver-detalhes"]');
-    if (!botao | !quadro.contains(botao)) return;
-    const cartao = botao.closest("[data-tarefa-id]");
-    const tarefa = tarefas.find((item) => item.id = cartao .dataset.tarefaId);
-    if (!tarefa) return;
-    console.log("Detalhes da tarefa:", tarefa);
-});
+        console.log(card.class);
+        
+
+        if (card.classList.contains("concluido")) {
+            console.log("entrou no if concluido");
+            quadroConcluido.appendChild(li);
+
+        } else if(card.classList.contains("em_progresso")) {
+            quadroEmAndamento.appendChild(li);
+
+        } else if(card.classList.contains("em_revisao")) {
+            quadroEmRevisao.append(li);
+
+        } else if(card.classList.contains("a_fazer")) {
+            quadroAfazer.appendChild(li);
+        }
+    }
+        
+}
+*/
 
 
-
-const cartoes = tarefas.map((e) => criarCartao(e));
-
-
-
-
-console.log(lista.children.length, cartoes.length);
